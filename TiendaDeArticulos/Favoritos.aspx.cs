6a159b69c31923eeee0e23a323ff9e607d798d43
JulioMaine <julioaamaine@gmail.com>
2023-10-18 19:36:37 -0300

    ...

--- TiendaDeArticulos/Favoritos.aspx.cs
+++ TiendaDeArticulos/Favoritos.aspx.cs
@@ -11,29 +11,8 @@ namespace TiendaDeArticulos
 {
     public partial class Favoritos : System.Web.UI.Page
     {
-        public List<Articulo> listaFavoritos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!Seguridad.sesionActiva(Session["user"]))
-            {
-                Response.Redirect("Login.aspx", false);
-                return;
-            }
-
-            if (Request.QueryString["Id"] != null)
-            {
-                listaFavoritos = new List<Articulo>();
-                ArticuloNegocio negocio = new ArticuloNegocio();
-                string id = Request.QueryString["Id"];
-                List<Articulo> articuloSeleccionado = new List<Articulo>();    
-                articuloSeleccionado = negocio.listar(id);
-                Articulo articulo = new Articulo();
-                articulo = articuloSeleccionado[0];
-                listaFavoritos.Add(articulo);
-            }
-
-            repRepetidor.DataSource = listaFavoritos;
-            repRepetidor.DataBind();
         }
     }
 }
---
+++ dominio/Favorito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    class Favorito
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public int  IdArticulo { get; set; }
+    }
+}
